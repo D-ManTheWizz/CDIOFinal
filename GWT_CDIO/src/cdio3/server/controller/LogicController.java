3058180cea2f12e4ed420ac63d33c0aff36d4702
Dannie M. Fjordside <Dannie_Fjordside@hotmail.com>
2016-06-09 15:08:09 +0200

    Moved Methods for generatingPass, from MainController to LogicController.

--- GWT_CDIO/src/cdio3/server/controller/LogicController.java
+++ GWT_CDIO/src/cdio3/server/controller/LogicController.java
@@ -1,5 +1,132 @@
 package cdio3.server.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class LogicController {
+	
+	private ArrayList<String> password = new ArrayList<String>();
+	
+	public String generatePassword() {
+		
+		// create a list of numbers 1-8       
+		ArrayList<Integer> a = new ArrayList<Integer>();
+		a.add(1);
+		a.add(2);
+		a.add(3);
+		a.add(4);
+		a.add(5);
+		a.add(6);
+		a.add(7);
+		a.add(8);
+		
+		// shuffle the list
+		Collections.shuffle(a); 
+		
+		for(int i=0; i<=7; i++) {
+			
+			// iterate through the shuffled list
+			int choice = a.get(i); 
+			
+			// run all 8 cases in random order
+			switch(choice) {
+			
+				// get a random UpperCase letter and add to the StringArray: password
+				case 1: getUppercase();
+						break;
+						
+				// get a random	LowerCase letter and add to the StringArray: password
+				case 2: getLowercase();
+						break;
+				
+				// get a random integer and add to the StringArray: password
+				case 3: getInt();
+						break;
+						
+				// get a random special-sign and add to the StringArray: password
+				case 4: getSpecial();
+						break;
+						
+				// get a random letter/int/sign and add to the StringArray: password
+				case 5: getRandom();
+						break;
+						
+				// get a random letter/int/sign and add to the StringArray: password
+				case 6: getRandom();
+						break;
+				
+				// get a random letter/int/sign and add to the StringArray: password
+				case 7: getRandom();
+						break;
+				
+				// get a random letter/int/sign and add to the StringArray: password
+				case 8: getRandom();
+						break;
+									
+			}	
+		}
+		
+		// generate the password from the StringArray: password
+		String generatedPassword = 	password.get(0) + password.get(1) + 
+									password.get(2) + password.get(3) + 
+									password.get(4) + password.get(5) + 
+									password.get(6) + password.get(7);
+
+		return generatedPassword;
+				      
+	}
+	
+	private void getUppercase() {
+		
+		String[] upper = {"A","B","C","D","E","F","G","H","I","J","K","L",
+				"M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+		Collections.shuffle(Arrays.asList(upper));
+		
+		password.add(upper[0]);
+		
+	}
+	
+	private void getLowercase() {
+		
+		String[] lower = {"a","b","c","d","e","f","g","h","i","j","k","l",
+				"m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+		Collections.shuffle(Arrays.asList(lower));
+		
+		password.add(lower[0]);
+		
+	}
+	
+	private void getInt() {
+		
+		String[] number = {"0","1","2","3","4","5","6","7","8","9"};
+		Collections.shuffle(Arrays.asList(number));
+		
+		password.add(number[0]);
+		
+	}
+	
+	private void getSpecial() {
+		
+		String[] special = {".", ",", "-", "_", "+", "!", "?", "="};
+		Collections.shuffle(Arrays.asList(special));
+		
+		password.add(special[0]);
+		
+	}
+	
+	private void getRandom() {
+		
+		String[] random = {".", ",", "-", "_", "+", "!", "?", "=","0","1",
+				"2","3","4","5","6","7","8","9","a","b","c","d","e","f","g",
+				"h","i","j","k","l","m","n","o","p","q","r","s","t","u","v",
+				"w","x","y","z","A","B","C","D","E","F","G","H","I","J","K",
+				"L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+		Collections.shuffle(Arrays.asList(random));
+		
+		password.add(random[0]);
+		
+	}
+	
 
 }
--- GWT_CDIO/src/cdio3/server/controller/MainController.java
+++ GWT_CDIO/src/cdio3/server/controller/MainController.java
@@ -7,6 +7,7 @@ import java.util.Collections;
 import java.util.Iterator;
 
 
+@SuppressWarnings("unused")
 public class MainController {
 
 //	MySQLOperatorDAO oprDAO = new MySQLOperatorDAO();
@@ -18,9 +19,6 @@ public class MainController {
 //	MySQLRaavareDAO rDAO = new MySQLRaavareDAO();
 	
 //	OperatoerDTO oprDTO;
-//	OperatoerDTO oprDTO2;
-	
-	private ArrayList<String> password = new ArrayList<String>();
 	
 	public MainController() {
 
@@ -116,130 +114,11 @@ public class MainController {
 		// make 4 char initials, from the first two char in first name and last name
 		String ini = firstName.substring(0, 2) + lastName.substring(0, 2);
 		
-		generatePassword();
+		LogicController logCon = new LogicController();
+		String password = logCon.generatePassword();
 		
-//		OperatoerDTO oprDTO = new OperatoerDTO(id, oprName, ini, cpr, this.password, stilling);
+//		OperatoerDTO oprDTO = new OperatoerDTO(id, oprName, ini, cpr, password, stilling);
 //		oprDAO.createOperatoer(oprDTO);
-	}
-	
-	private String generatePassword() {
-		
-		// create a list of numbers 1-8       
-		ArrayList<Integer> a = new ArrayList<Integer>();
-		a.add(1);
-		a.add(2);
-		a.add(3);
-		a.add(4);
-		a.add(5);
-		a.add(6);
-		a.add(7);
-		a.add(8);
-		
-		// shuffle the list
-		Collections.shuffle(a); 
-		
-		for(int i=0; i<=7; i++) {
-			
-			// iterate through the shuffled list
-			int choice = a.get(i); 
-			
-			// run all 8 cases in random order
-			switch(choice) {
-			
-				// get a random UpperCase letter and add to the StringArray: password
-				case 1: getUppercase();
-						break;
-						
-				// get a random	LowerCase letter and add to the StringArray: password
-				case 2: getLowercase();
-						break;
-				
-				// get a random integer and add to the StringArray: password
-				case 3: getInt();
-						break;
-						
-				// get a random special-sign and add to the StringArray: password
-				case 4: getSpecial();
-						break;
-						
-				// get a random letter/int/sign and add to the StringArray: password
-				case 5: getRandom();
-						break;
-						
-				// get a random letter/int/sign and add to the StringArray: password
-				case 6: getRandom();
-						break;
-				
-				// get a random letter/int/sign and add to the StringArray: password
-				case 7: getRandom();
-						break;
-				
-				// get a random letter/int/sign and add to the StringArray: password
-				case 8: getRandom();
-						break;
-									
-			}	
-		}
-		
-		// generate the password from the StringArray: password
-		String generatedPassword = 	password.get(0) + password.get(1) + 
-									password.get(2) + password.get(3) + 
-									password.get(4) + password.get(5) + 
-									password.get(6) + password.get(7);
-
-		return generatedPassword;
-				      
-	}
-	
-	private void getUppercase() {
-		
-		String[] upper = {"A","B","C","D","E","F","G","H","I","J","K","L",
-				"M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
-		Collections.shuffle(Arrays.asList(upper));
-		
-		password.add(upper[0]);
-		
-	}
-	
-	private void getLowercase() {
-		
-		String[] lower = {"a","b","c","d","e","f","g","h","i","j","k","l",
-				"m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
-		Collections.shuffle(Arrays.asList(lower));
-		
-		password.add(lower[0]);
-		
-	}
-	
-	private void getInt() {
-		
-		String[] number = {"0","1","2","3","4","5","6","7","8","9"};
-		Collections.shuffle(Arrays.asList(number));
-		
-		password.add(number[0]);
-		
-	}
-	
-	private void getSpecial() {
-		
-		String[] special = {".", ",", "-", "_", "+", "!", "?", "="};
-		Collections.shuffle(Arrays.asList(special));
-		
-		password.add(special[0]);
-		
-	}
-	
-	private void getRandom() {
-		
-		String[] random = {".", ",", "-", "_", "+", "!", "?", "=","0","1",
-				"2","3","4","5","6","7","8","9","a","b","c","d","e","f","g",
-				"h","i","j","k","l","m","n","o","p","q","r","s","t","u","v",
-				"w","x","y","z","A","B","C","D","E","F","G","H","I","J","K",
-				"L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
-		Collections.shuffle(Arrays.asList(random));
-		
-		password.add(random[0]);
-		
 	}
 	
 	public String/*OperatorDTO*/ getOperator(int oprID) {
