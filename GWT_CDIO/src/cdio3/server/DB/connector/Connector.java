22460a4b15b8843dc86d935e3a846cb7a36d62b5
Dannie M. Fjordside <Dannie_Fjordside@hotmail.com>
2016-06-10 17:41:37 +0200

    Added Connector to DB part

---
+++ GWT_CDIO/src/cdio3/server/DB/connector/Connector.java
@@ -0,0 +1,49 @@
+package cdio3.server.DB.connector;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/** @author Ronnie Dalsgaard */
+public class Connector {
+	private final String HOST     = "ec2-52-30-89-247.eu-west-1.compute.amazonaws.com";
+    private final int    PORT     = 3306;
+    private final String DATABASE = "grp21";
+    private final String USERNAME = "grp21"; 
+    private final String PASSWORD = "xH^v#6c=";
+		
+//    private final String HOST     = "Localhost";
+//    private final int    PORT     = 3306;
+//    private final String DATABASE = "Projekt";
+//    private final String USERNAME = "root"; 
+//    private final String PASSWORD = "";
+    private static Connection connection;
+    
+    public Connector() {
+        try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE;
+			connection = DriverManager.getConnection(url, USERNAME, PASSWORD);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+    }
+    
+    public Connection getConnection(){
+    	return connection;
+    }
+    
+    public ResultSet doQuery(String query) throws SQLException{
+        Statement stmt = connection.createStatement();
+        ResultSet res = stmt.executeQuery(query);
+        return res;
+    }
+    
+    public void doUpdate(String query) throws SQLException{
+        Statement stmt = connection.createStatement();
+        stmt.executeUpdate(query);
+    }
+}
