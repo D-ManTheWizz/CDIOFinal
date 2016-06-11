93cc72db315ad7512317ac1035b9d1dbb48dd7b3
Dannie M. Fjordside <Dannie_Fjordside@hotmail.com>
2016-06-11 18:13:53 +0200

    Bug-fix DALException

--- GWT_CDIO/src/cdio3/shared/DALException.java
+++ GWT_CDIO/src/cdio3/shared/DALException.java
@@ -1,8 +1,11 @@
 package cdio3.shared;
 
-public class DALException extends Exception
+import java.io.Serializable;
+
+public class DALException extends Exception implements Serializable
 {
 	private static final long serialVersionUID = -5490114628089339322L;
+	public DALException() {}
 	public DALException(String message) { super(message); }    
 	public DALException(Exception e) { super(e); }
 }
