233dc8841c7e501a0e694c85f3681c2a14ab0476
Dannie M. Fjordside <Dannie_Fjordside@hotmail.com>
2016-06-16 14:25:48 +0200

    LoginMenu Done - trimmed code and removed unused code

--- GWT_CDIO/src/cdio3/client/gui/Login.java
+++ GWT_CDIO/src/cdio3/client/gui/Login.java
@@ -5,21 +5,16 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class Login extends Composite {
 	private VerticalPanel vPanel = new VerticalPanel();
-	private VerticalPanel contentPanel;
 	
 	private MainView main;
 	
 	public Login(MainView main){
-		this.main = main; // Login gets opened by Main, not Main opened by Login
+		this.main = main;
 		
 		initWidget(this.vPanel);
 		this.vPanel.setBorderWidth(1);
 		
-		
 		LoginView menu = new LoginView(this.main);
-		this.vPanel.add(menu);
-		
-		this.contentPanel = new VerticalPanel();
-		this.vPanel.add(contentPanel);			
+		this.vPanel.add(menu);		
 	}
 }
