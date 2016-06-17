7d7f56005d89ee05dc4f343bf349547fb4c1fe16
Haydar Tas <Haydar@10.16.167.17>
2016-06-17 10:22:14 +0200

    .

--- GWT_CDIO/src/cdio3/server/DB/test/TestProduktBatchKomponentDAO.java
+++ GWT_CDIO/src/cdio3/server/DB/test/TestProduktBatchKomponentDAO.java
@@ -16,59 +16,59 @@ import cdio3.shared.ProduktBatchKompDTO;
 
 public class TestProduktBatchKomponentDAO {
 	ProduktBatchKompDAO pbkDAO = new MySQLProduktBatchKompDAO();
-	
+
 	@Before
 	public void connect()
 	{
 		try {
 			new Connector();
 		} catch (Exception e) {
-		
+
 		}
 	}
 
 	@Test
 	public void testGetProduktBachKomp() throws DALException {
-		
+
 		List<ProduktBatchKompDTO> pbkDTO = pbkDAO.getProduktBatchKompList();
 		int validId = pbkDAO.getProduktBatchKompList().get(0).getPbId();
-		
+
 		pbkDAO.getProduktBatchKompList(validId);
 		pbkDTO.get(0);
-		
+
 		boolean sameEelement = true;
-		
+
 		assertTrue(sameEelement);
 
-		
+
 	}
 
 
 	@Test
 	public void testgetProdukBatchKompListPbId() throws DALException {
-		
-		
+
+
 		List<ProduktBatchKompDTO> list = pbkDAO.getProduktBatchKompList(1);
-		
+
 		assertTrue(list.size()>1);
-		
+
 	}
 
-	
+
 	@Test
 	public void testProduktBatchKompList() throws DALException {
 
-		
-List<ProduktBatchKompDTO> list = pbkDAO.getProduktBatchKompList();
-		
+
+		List<ProduktBatchKompDTO> list = pbkDAO.getProduktBatchKompList();
+
 		assertTrue(list.size()>1);	
-	
+
 	}
-	
-	
+
+
 	@Test
 	public void testUpdateProduktBatchKomp() throws DALException {
-		
+
 		ProduktBatchKompDTO pbkDTO = null;
 		double expected = 1.8;
 		try {
@@ -77,10 +77,10 @@ List<ProduktBatchKompDTO> list = pbkDAO.getProduktBatchKompList();
 			pbkDAO.updateProduktBatchKomp(pbkDTO);
 		} catch (DALException e) {
 			e.printStackTrace();
-			
+
 		}
 		double actual = pbkDTO.getNetto();
-		
+
 		assertEquals(expected, actual, 0);
 	}
-	
+}	
